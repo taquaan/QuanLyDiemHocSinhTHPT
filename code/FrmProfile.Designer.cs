2e4c7f1df21329c5313658379cc5310995b8fd89
NguyenHongQuan1208 <nquan1208@gmail.com>
2024-10-30 19:03:49 +0700

    Update Profile UI

--- code/FrmProfile.Designer.cs
+++ code/FrmProfile.Designer.cs
@@ -29,12 +29,237 @@ namespace QuanLyDiemHocSinhTHPT
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "FrmProfile";
+            System.Windows.Forms.ListBox listBox1;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelMaGV = new System.Windows.Forms.Label();
+            this.ProfileTitle = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
+            listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelMaGV);
+            this.panel1.Location = new System.Drawing.Point(33, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 43);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // labelMaGV
+            // 
+            this.labelMaGV.AutoSize = true;
+            this.labelMaGV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaGV.Location = new System.Drawing.Point(3, 12);
+            this.labelMaGV.Name = "labelMaGV";
+            this.labelMaGV.Size = new System.Drawing.Size(127, 20);
+            this.labelMaGV.TabIndex = 0;
+            this.labelMaGV.Text = "Mã giáo viên: ";
+            this.labelMaGV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelMaGV.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ProfileTitle
+            // 
+            this.ProfileTitle.AutoSize = true;
+            this.ProfileTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProfileTitle.Location = new System.Drawing.Point(27, 57);
+            this.ProfileTitle.Name = "ProfileTitle";
+            this.ProfileTitle.Size = new System.Drawing.Size(259, 32);
+            this.ProfileTitle.TabIndex = 1;
+            this.ProfileTitle.Text = "Thông tin cá nhân";
+            this.ProfileTitle.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(33, 153);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(275, 43);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Họ và Tên:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dateTimePicker1);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(33, 202);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(369, 43);
+            this.panel3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ngày sinh: ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(listBox1);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(33, 251);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(253, 43);
+            this.panel4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(136, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Giới tính: ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 16;
+            listBox1.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            listBox1.Location = new System.Drawing.Point(143, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(120, 84);
+            listBox1.TabIndex = 2;
+            listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Location = new System.Drawing.Point(21, 8);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(80, 23);
+            this.BtnUpdate.TabIndex = 4;
+            this.BtnUpdate.Text = "Cập nhật";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.BtnExit);
+            this.panel5.Controls.Add(this.BtnUpdate);
+            this.panel5.Location = new System.Drawing.Point(502, 96);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(135, 187);
+            this.panel5.TabIndex = 5;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(21, 46);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 23);
+            this.BtnExit.TabIndex = 5;
+            this.BtnExit.Text = "Thoát";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // FrmProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 451);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.ProfileTitle);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông tin cá nhân";
+            this.Load += new System.EventHandler(this.FrmProfile_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
-    #endregion
-  }
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelMaGV;
+        private System.Windows.Forms.Label ProfileTitle;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button BtnExit;
+    }
 }
--- code/FrmProfile.cs
+++ code/FrmProfile.cs
@@ -16,5 +16,50 @@ namespace QuanLyDiemHocSinhTHPT
     {
       InitializeComponent();
     }
-  }
+
+        private void FrmProfile_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
