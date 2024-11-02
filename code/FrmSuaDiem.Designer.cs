8386c914f5511c85ba67ce5e4c6e952ae798d90a
taquan <tnaquan@gmail.com>
2024-11-03 00:15:31 +0700

    Thêm chức năng chính cho Form sửa điểm

--- code/FrmSuaDiem.Designer.cs
+++ code/FrmSuaDiem.Designer.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace QuanLyDiemHocSinhTHPT
 {
   partial class FrmSuaDiem
@@ -30,32 +30,212 @@ namespace QuanLyDiemHocSinhTHPT
     private void InitializeComponent()
     {
             this.dtaThongTin = new System.Windows.Forms.DataGridView();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.labelMaHocSinh = new System.Windows.Forms.Label();
+            this.cboMaHocSinh = new System.Windows.Forms.ComboBox();
+            this.labelHocKy = new System.Windows.Forms.Label();
+            this.cboMon = new System.Windows.Forms.ComboBox();
+            this.labelMon = new System.Windows.Forms.Label();
+            this.txtDiemGiuaKy = new System.Windows.Forms.TextBox();
+            this.labelDiemGiuaKy = new System.Windows.Forms.Label();
+            this.txtDiemCuoiKy = new System.Windows.Forms.TextBox();
+            this.labelDiemCuoiKy = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.labelDashboard = new System.Windows.Forms.Label();
+            this.cboHocKy = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtaThongTin)).BeginInit();
             this.SuspendLayout();
             // 
             // dtaThongTin
             // 
             this.dtaThongTin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtaThongTin.Location = new System.Drawing.Point(54, 142);
+            this.dtaThongTin.Location = new System.Drawing.Point(36, 291);
             this.dtaThongTin.Name = "dtaThongTin";
-            this.dtaThongTin.Size = new System.Drawing.Size(658, 181);
+            this.dtaThongTin.Size = new System.Drawing.Size(1011, 217);
             this.dtaThongTin.TabIndex = 0;
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.Location = new System.Drawing.Point(29, 61);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(282, 37);
+            this.mainLabel.TabIndex = 1;
+            this.mainLabel.Text = "Sửa điểm học sinh";
+            // 
+            // labelMaHocSinh
+            // 
+            this.labelMaHocSinh.AutoSize = true;
+            this.labelMaHocSinh.Location = new System.Drawing.Point(36, 113);
+            this.labelMaHocSinh.Name = "labelMaHocSinh";
+            this.labelMaHocSinh.Size = new System.Drawing.Size(68, 13);
+            this.labelMaHocSinh.TabIndex = 2;
+            this.labelMaHocSinh.Text = "Mã học sinh:";
+            // 
+            // cboMaHocSinh
+            // 
+            this.cboMaHocSinh.FormattingEnabled = true;
+            this.cboMaHocSinh.Location = new System.Drawing.Point(111, 113);
+            this.cboMaHocSinh.Name = "cboMaHocSinh";
+            this.cboMaHocSinh.Size = new System.Drawing.Size(604, 21);
+            this.cboMaHocSinh.TabIndex = 3;
+            // 
+            // labelHocKy
+            // 
+            this.labelHocKy.AutoSize = true;
+            this.labelHocKy.Location = new System.Drawing.Point(36, 164);
+            this.labelHocKy.Name = "labelHocKy";
+            this.labelHocKy.Size = new System.Drawing.Size(45, 13);
+            this.labelHocKy.TabIndex = 6;
+            this.labelHocKy.Text = "Học Kỳ:";
+            // 
+            // cboMon
+            // 
+            this.cboMon.FormattingEnabled = true;
+            this.cboMon.Location = new System.Drawing.Point(111, 190);
+            this.cboMon.Name = "cboMon";
+            this.cboMon.Size = new System.Drawing.Size(604, 21);
+            this.cboMon.TabIndex = 9;
+            // 
+            // labelMon
+            // 
+            this.labelMon.AutoSize = true;
+            this.labelMon.Location = new System.Drawing.Point(36, 190);
+            this.labelMon.Name = "labelMon";
+            this.labelMon.Size = new System.Drawing.Size(52, 13);
+            this.labelMon.TabIndex = 8;
+            this.labelMon.Text = "Môn học:";
+            // 
+            // txtDiemGiuaKy
+            // 
+            this.txtDiemGiuaKy.Location = new System.Drawing.Point(111, 217);
+            this.txtDiemGiuaKy.Name = "txtDiemGiuaKy";
+            this.txtDiemGiuaKy.Size = new System.Drawing.Size(258, 20);
+            this.txtDiemGiuaKy.TabIndex = 11;
+            // 
+            // labelDiemGiuaKy
+            // 
+            this.labelDiemGiuaKy.AutoSize = true;
+            this.labelDiemGiuaKy.Location = new System.Drawing.Point(36, 220);
+            this.labelDiemGiuaKy.Name = "labelDiemGiuaKy";
+            this.labelDiemGiuaKy.Size = new System.Drawing.Size(46, 13);
+            this.labelDiemGiuaKy.TabIndex = 10;
+            this.labelDiemGiuaKy.Text = "Giữa kỳ:";
+            // 
+            // txtDiemCuoiKy
+            // 
+            this.txtDiemCuoiKy.Location = new System.Drawing.Point(426, 217);
+            this.txtDiemCuoiKy.Name = "txtDiemCuoiKy";
+            this.txtDiemCuoiKy.Size = new System.Drawing.Size(289, 20);
+            this.txtDiemCuoiKy.TabIndex = 13;
+            // 
+            // labelDiemCuoiKy
+            // 
+            this.labelDiemCuoiKy.AutoSize = true;
+            this.labelDiemCuoiKy.Location = new System.Drawing.Point(375, 220);
+            this.labelDiemCuoiKy.Name = "labelDiemCuoiKy";
+            this.labelDiemCuoiKy.Size = new System.Drawing.Size(45, 13);
+            this.labelDiemCuoiKy.TabIndex = 12;
+            this.labelDiemCuoiKy.Text = "Cuối kỳ:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(734, 113);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(313, 52);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(734, 176);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(313, 28);
+            this.btnSua.TabIndex = 15;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(734, 211);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(313, 28);
+            this.btnXoa.TabIndex = 16;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // labelDashboard
+            // 
+            this.labelDashboard.AutoSize = true;
+            this.labelDashboard.Location = new System.Drawing.Point(36, 37);
+            this.labelDashboard.Name = "labelDashboard";
+            this.labelDashboard.Size = new System.Drawing.Size(73, 13);
+            this.labelDashboard.TabIndex = 17;
+            this.labelDashboard.Text = "Về Trang Chủ";
+            this.labelDashboard.Click += new System.EventHandler(this.labelDashboard_Click);
+            // 
+            // cboHocKy
+            // 
+            this.cboHocKy.FormattingEnabled = true;
+            this.cboHocKy.Location = new System.Drawing.Point(111, 164);
+            this.cboHocKy.Name = "cboHocKy";
+            this.cboHocKy.Size = new System.Drawing.Size(604, 21);
+            this.cboHocKy.TabIndex = 18;
             // 
             // FrmSuaDiem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1096, 551);
+            this.Controls.Add(this.cboHocKy);
+            this.Controls.Add(this.labelDashboard);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDiemCuoiKy);
+            this.Controls.Add(this.labelDiemCuoiKy);
+            this.Controls.Add(this.txtDiemGiuaKy);
+            this.Controls.Add(this.labelDiemGiuaKy);
+            this.Controls.Add(this.cboMon);
+            this.Controls.Add(this.labelMon);
+            this.Controls.Add(this.labelHocKy);
+            this.Controls.Add(this.cboMaHocSinh);
+            this.Controls.Add(this.labelMaHocSinh);
+            this.Controls.Add(this.mainLabel);
             this.Controls.Add(this.dtaThongTin);
             this.Name = "FrmSuaDiem";
             this.Text = "FrmSuaDiem";
+            this.Load += new System.EventHandler(this.FrmSuaDiem_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtaThongTin)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.DataGridView dtaThongTin;
+    private System.Windows.Forms.Label mainLabel;
+    private System.Windows.Forms.Label labelMaHocSinh;
+    private System.Windows.Forms.ComboBox cboMaHocSinh;
+    private System.Windows.Forms.Label labelHocKy;
+    private System.Windows.Forms.ComboBox cboMon;
+    private System.Windows.Forms.Label labelMon;
+    private System.Windows.Forms.TextBox txtDiemGiuaKy;
+    private System.Windows.Forms.Label labelDiemGiuaKy;
+    private System.Windows.Forms.TextBox txtDiemCuoiKy;
+    private System.Windows.Forms.Label labelDiemCuoiKy;
+    private System.Windows.Forms.Button btnAdd;
+    private System.Windows.Forms.Button btnSua;
+    private System.Windows.Forms.Button btnXoa;
+    private System.Windows.Forms.Label labelDashboard;
+    private System.Windows.Forms.ComboBox cboHocKy;
   }
 }
--- code/FrmSuaDiem.cs
+++ code/FrmSuaDiem.cs
@@ -17,5 +17,148 @@ namespace QuanLyDiemHocSinhTHPT
     {
       InitializeComponent();
     }
+
+    // LOAD DATA
+    private void LoadData()
+    {
+      string sql = "SELECT * FROM Diem";
+      DataTable dt = kn.Lay_Dulieu(sql); 
+      dtaThongTin.DataSource = dt;
+    }
+    private void LoadMaHocSinh()
+    {
+      string sql = "SELECT MaHocSinh FROM HocSinh";
+      DataTable dtHosInh = kn.Lay_Dulieu(sql);
+
+      cboMaHocSinh.DataSource = dtHosInh;
+      cboMaHocSinh.DisplayMember = "MaHocSinh"; 
+      cboMaHocSinh.ValueMember = "MaHocSinh";
+    }
+    private void LoadHocKy()
+    {
+      string sql = "SELECT MaHocKy, TenHocKy FROM HocKy";
+      DataTable dtHocKy = kn.Lay_Dulieu(sql);
+
+      // Thêm 1 cột tạm thời để hiển thị tên cùng với mã  
+      dtHocKy.Columns.Add("DisplayText", typeof(string));
+      foreach (DataRow row in dtHocKy.Rows)
+      {
+        row["DisplayText"] = $"{row["TenHocKy"]} (Mã: {row["MaHocKy"]})";
+      }
+
+      cboHocKy.DataSource = dtHocKy;
+      cboHocKy.DisplayMember = "DisplayText";
+      cboHocKy.ValueMember = "MaHocKy";
+    }
+    private void LoadMon()
+    {
+      string sql = "SELECT MaMonHoc, TenMonHoc FROM MonHoc";
+      DataTable dtMon = kn.Lay_Dulieu(sql);
+
+      dtMon.Columns.Add("DisplayText", typeof(string));
+      foreach (DataRow row in dtMon.Rows)
+      {
+        row["DisplayText"] = $"{row["TenMonHoc"]} (Mã: {row["MaMonHoc"]})";
+      }
+
+      cboMon.DataSource = dtMon;
+      cboMon.DisplayMember = "DisplayText";
+      cboMon.ValueMember = "MaMonHoc";
+    }
+
+    // VALIDATION
+    private bool gradeisEmpty()
+    {
+      if (string.IsNullOrWhiteSpace(txtDiemGiuaKy.Text) || string.IsNullOrWhiteSpace(txtDiemCuoiKy.Text))
+      {
+        MessageBox.Show("Nhập cả điểm Giữa Kỳ và Cuối Kỳ.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        return true;
+      }
+      return false;
+    }
+
+    private bool instanceExists()
+    {
+      string mahs = cboMaHocSinh.Text;
+      string hocKy = cboHocKy.SelectedValue.ToString();
+      string mon = cboMon.SelectedValue.ToString();
+
+      string sql = $"SELECT COUNT(*) FROM Diem WHERE MaHocSinh = '{mahs}' AND MaHocKy = '{hocKy}' AND MaMonHoc = '{mon}'";
+      DataTable dt = kn.Lay_Dulieu(sql);
+
+      if (dt.Rows.Count > 0)
+      {
+        MessageBox.Show("Mã học sinh, học kỳ và môn học đã tồn tại. Vui lòng nhập lại.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        return true;
+      }
+      return false;
+    }
+
+    // EVENT
+    private void labelDashboard_Click(object sender, EventArgs e)
+    {
+      FrmDashboard dashboard = new FrmDashboard();
+      dashboard.Show();
+      this.Close();
+    }
+
+    private void FrmSuaDiem_Load(object sender, EventArgs e)
+    {
+      LoadData();
+      LoadMaHocSinh();
+      LoadHocKy();
+      LoadMon();
+    }
+
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+      if (gradeisEmpty())
+      {
+        return;
+      }
+      if (instanceExists())
+      {
+        return;
+      }
+
+      string mahs = cboMaHocSinh.Text;
+      string hocKy = cboHocKy.SelectedValue.ToString();
+      string mon = cboMon.SelectedValue.ToString();
+      float giuaKy = float.Parse(txtDiemGiuaKy.Text);
+      float cuoiKy = float.Parse(txtDiemCuoiKy.Text);
+
+      string sql = $"INSERT INTO Diem (DiemGiuaKy, DiemCuoiKy, MaHocSinh, MaMonHoc, MaHocKy) VALUES ('{giuaKy}', '{cuoiKy}', '{mahs}', '{mon}', '{hocKy}')";
+      kn.ThucThi(sql);
+      LoadData();
+    }
+
+    private void btnSua_Click(object sender, EventArgs e)
+    {
+      if (gradeisEmpty())
+      {
+        return;
+      }
+
+      string mahs = cboMaHocSinh.Text;
+      string hocKy = cboHocKy.SelectedValue.ToString();
+      string mon = cboMon.SelectedValue.ToString();
+      float giuaKy = float.Parse(txtDiemGiuaKy.Text);
+      float cuoiKy = float.Parse(txtDiemCuoiKy.Text);
+
+      string sql = $"UPDATE Diem SET DiemGiuaKy = '{giuaKy}', DiemCuoiKy = '{cuoiKy}' WHERE MaHocSinh = '{mahs}' AND MaMonHoc = '{mon}' AND MaHocKy = '{hocKy}'"; ;
+      kn.ThucThi(sql);
+      LoadData();
+    }
+
+    private void btnXoa_Click(object sender, EventArgs e)
+    {
+      string mahs = cboMaHocSinh.Text;
+      string hocKy = cboHocKy.SelectedValue.ToString();
+      string mon = cboMon.SelectedValue.ToString();
+
+      string sql = $"DELETE FROM Diem WHERE MaHocSinh = '{mahs}' AND MaMonHoc = '{mon}' AND MaHocKy = '{hocKy}'";
+      kn.ThucThi(sql);
+      LoadData();
+    }
   }
 }
